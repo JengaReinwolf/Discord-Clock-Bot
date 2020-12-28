d0b85e451c1df84eb3b8d71e898434f66132db68
Jenga <jenga@jenga.dog>
2020-12-27 22:37:06 -0600

    Genericize

--- clock_bot.py
+++ clock_bot.py
@@ -2,6 +2,7 @@ import json
 import pytz
 from asyncio import sleep
 from datetime import datetime
+from sys import stderr
 
 import discord
 from discord.ext import tasks
@@ -10,9 +11,13 @@ from discord.ext import tasks
 client = discord.Client()
 with open("config.json", "rb") as f:
     config = json.loads(f.read())
+SYNC_MINUTE = config["sync_minute"]
+if SYNC_MINUTE < 1:
+    print("Invalid sync minute", file=stderr)
+    exit(-1)
 
 
-@tasks.loop(minutes=5)
+@tasks.loop(minutes=SYNC_MINUTE)
 async def update_time_channels():
     await client.wait_until_ready()
     time_str = datetime.now(tz=pytz.timezone(config["timezone"])).strftime("%H:%M%p %Z")
@@ -20,13 +25,17 @@ async def update_time_channels():
         await client.get_channel(int(channel_id)).edit(name=f"🕒 ~{time_str}")
 
 
-def get_5m_sync_delay():
-    """Returns the delay in seconds to the next 5-minute sync point"""
+def get_sync_delay(sync_minute=5):
+    """
+        Returns the delay in seconds to the next sync point
+        Example:
+          5 would sync at 5, 10, 15, 20...
+    """
     current_dt = datetime.now()
     delay_seconds = 0
 
-    cminutes = 5 - (current_dt.minute % 5)  # Remaining minutes
-    if cminutes != 5:  # Add time if minutes remaining
+    cminutes = sync_minute - (current_dt.minute % sync_minute)  # Remaining minutes
+    if cminutes != sync_minute:  # Add time if minutes remaining
         delay_seconds += cminutes * 60
 
     delay_seconds += (
@@ -39,7 +48,7 @@ def get_5m_sync_delay():
 @update_time_channels.before_loop
 async def pre_update_time_channels():
     await client.wait_until_ready()
-    await sleep(get_5m_sync_delay())
+    await sleep(get_sync_delay(SYNC_MINUTE))
 
 
 update_time_channels.start()
